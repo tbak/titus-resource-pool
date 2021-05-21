66232668be5bbcb65ee15eb60afedf290dae0a1f
tbak <tbak@netflix.com>
2021-05-21 09:27:01 -0700

    Add buffer capacity group name resolver (#14)

--- reserved/constants.go
+++ reserved/constants.go
@@ -3,4 +3,12 @@ package reserved
 const (
 	PodSchedulerKube  = "kubeScheduler"
 	PodSchedulerFenzo = "fenzo"
+
+	bufferCapacityGroupSuffix = "buffer"
 )
+
+// GetBufferCapacityGroupName returns a name of a buffer capacity group given the resource pool name.
+// A buffer capacity group name format is <resource_pool_name>buffer.
+func GetBufferCapacityGroupName(resourcePoolName string) string {
+	return resourcePoolName + bufferCapacityGroupSuffix
+}
