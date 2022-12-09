231ea94d9c7e35d6b8612344fb458441176f483e
joshi-keyur <56899791+joshi-keyur@users.noreply.github.com>
2022-12-09 10:28:14 -0800

    add functions to get capacity usage only on active nodes (#44)

--- reserved/usage.go
+++ reserved/usage.go
@@ -1,8 +1,11 @@
 package reserved
 
 import (
+	"time"
+
 	capacityGroupV1 "github.com/Netflix/titus-controllers-api/api/capacitygroup/v1"
 	poolV1 "github.com/Netflix/titus-controllers-api/api/resourcepool/v1"
+	"github.com/Netflix/titus-resource-pool/node"
 	poolPod "github.com/Netflix/titus-resource-pool/pod"
 	"github.com/Netflix/titus-resource-pool/resourcepool"
 	v1 "k8s.io/api/core/v1"
@@ -22,7 +25,8 @@ type CapacityReservationUsage struct {
 	Buffer                         Usage
 	BufferAllocatedByCapacityGroup map[string]poolV1.ComputeResource
 	// Trough
-	TroughUsedReservedUnallocated poolV1.ComputeResource
+	TroughUsedReservedUnallocated              poolV1.ComputeResource
+	TroughUsedReservedUnallocatedOnActiveNodes poolV1.ComputeResource
 	// Elastic
 	Elastic                         Usage
 	ElasticAllocatedByCapacityGroup map[string]poolV1.ComputeResource
@@ -37,6 +41,86 @@ func (u Usage) Add(other Usage) Usage {
 		Allocated:      u.Allocated.Add(other.Allocated),
 		Unallocated:    u.Unallocated.Add(other.Unallocated),
 		OverAllocation: u.OverAllocation.Add(other.OverAllocation),
+	}
+}
+
+func NewActiveCapacityReservationUsage(snapshot *resourcepool.ResourceSnapshot,
+	reservations []*capacityGroupV1.CapacityGroup, bufferName string) *CapacityReservationUsage {
+	inCapacityGroup := map[string]Usage{}
+	bufferAllocatedByCapacityGroup := map[string]poolV1.ComputeResource{}
+	elasticAllocatedByCapacityGroup := map[string]poolV1.ComputeResource{}
+	allReserved := Usage{}
+
+	var bufferCapacityGroup *capacityGroupV1.CapacityGroup
+	for _, reservation := range reservations {
+		if reservation.Name == bufferName {
+			bufferCapacityGroup = reservation
+			break
+		}
+	}
+
+	var bufferShape poolV1.ComputeResource
+	var bufferTotal poolV1.ComputeResource
+	if bufferCapacityGroup != nil {
+		bufferShape = bufferCapacityGroup.Spec.ComputeResource
+		bufferTotal = bufferShape.Multiply(int64(bufferCapacityGroup.Spec.InstanceCount))
+	}
+	remainingBuffer := bufferTotal
+
+	totalBufferOverallocation := poolV1.Zero
+	totalElasticAllocation := poolV1.Zero
+	for _, reservation := range reservations {
+		if reservation.Spec.ResourcePoolName == snapshot.ResourcePoolName {
+			if reservation.Name != bufferName {
+				usage, overallocatedPods := buildActiveUsage(snapshot, reservation)
+				reservationName := reservation.Spec.OriginalName
+				inCapacityGroup[reservationName] = usage
+
+				allReserved.Allocated = allReserved.Allocated.Add(usage.Allocated)
+				allReserved.Unallocated = allReserved.Unallocated.Add(usage.Unallocated)
+				allReserved.OverAllocation = allReserved.OverAllocation.Add(usage.OverAllocation)
+
+				bufferAllocated, bufferOverallocation, elasticAllocated := buildBufferAndElasticUsage(remainingBuffer, overallocatedPods)
+				bufferAllocatedByCapacityGroup[reservationName] = bufferAllocated
+				elasticAllocatedByCapacityGroup[reservationName] = elasticAllocated
+				remainingBuffer = remainingBuffer.Sub(bufferAllocated)
+				totalBufferOverallocation = totalBufferOverallocation.Add(bufferOverallocation)
+				totalElasticAllocation = totalElasticAllocation.Add(elasticAllocated)
+			}
+		}
+	}
+
+	var bufferUsage Usage
+	if bufferCapacityGroup != nil {
+		bufferUsage.Allocated = bufferTotal.SubWithLimit(remainingBuffer, 0)
+		bufferUsage.Unallocated = remainingBuffer
+		bufferUsage.OverAllocation = totalBufferOverallocation
+
+		allReserved.Allocated = allReserved.Allocated.Add(bufferUsage.Allocated)
+		allReserved.Unallocated = allReserved.Unallocated.Add(bufferUsage.Unallocated)
+		allReserved.OverAllocation = totalBufferOverallocation
+	}
+
+	troughUsedReservedUnallocated := buildTroughUsage(snapshot)
+	troughUsedReservedUnallocatedOnActiveNodes := buildTroughUsageOnActiveNodes(snapshot)
+
+	// only use active nodes' allocatable capacity as aggregate
+	totalElastic := node.SumNodeResourcesInMap(snapshot.NodeSnapshot.ActiveByName).
+		SubWithLimit(allReserved.Allocated.Add(allReserved.Unallocated), 0)
+	elasticUsage := Usage{
+		Allocated:   totalElasticAllocation,
+		Unallocated: totalElastic.SubWithLimit(totalElasticAllocation, 0),
+	}
+
+	return &CapacityReservationUsage{
+		InCapacityGroup:                inCapacityGroup,
+		Buffer:                         bufferUsage,
+		BufferAllocatedByCapacityGroup: bufferAllocatedByCapacityGroup,
+		TroughUsedReservedUnallocated:  troughUsedReservedUnallocated,
+		TroughUsedReservedUnallocatedOnActiveNodes: troughUsedReservedUnallocatedOnActiveNodes,
+		Elastic:                         elasticUsage,
+		ElasticAllocatedByCapacityGroup: elasticAllocatedByCapacityGroup,
+		AllReserved:                     allReserved,
 	}
 }
 
@@ -121,6 +205,41 @@ func NewCapacityReservationUsage(snapshot *resourcepool.ResourceSnapshot,
 	}
 }
 
+func buildActiveUsage(snapshot *resourcepool.ResourceSnapshot, reservation *capacityGroupV1.CapacityGroup) (Usage, []*v1.Pod) {
+	reservedResources := CapacityGroupResources(reservation)
+	allocated := poolV1.ComputeResource{}
+	overAllocated := poolV1.ComputeResource{}
+	overAllocationPods := []*v1.Pod{}
+	for _, pod := range snapshot.PodSnapshot.ScheduledByName {
+		if poolPod.IsPodPreemptible(pod) {
+			continue
+		}
+		if poolPod.IsPodRunning(pod) && poolPod.IsPodInCapacityGroup(pod, reservation) {
+			var n *v1.Node
+			var exists bool
+			// since the pod is already running, the node age does not matter for demand accounting
+			if n, exists = snapshot.NodeSnapshot.ActiveByName[pod.Spec.NodeName]; !exists ||
+				!node.IsNodeAvailableForScheduling(n, time.Now(), 0) {
+				continue
+			}
+			podResources := poolPod.FromPodToComputeResource(pod)
+			nextAllocated := allocated.Add(podResources)
+			if nextAllocated != reservedResources && !nextAllocated.LessThan(reservedResources) {
+				overAllocated = overAllocated.Add(podResources)
+				overAllocationPods = append(overAllocationPods, pod)
+			} else {
+				allocated = nextAllocated
+			}
+		}
+	}
+
+	return Usage{
+		Allocated:      allocated,
+		Unallocated:    reservedResources.SubWithLimit(allocated, 0),
+		OverAllocation: overAllocated,
+	}, overAllocationPods
+}
+
 func buildUsage(snapshot *resourcepool.ResourceSnapshot, reservation *capacityGroupV1.CapacityGroup) (Usage, []*v1.Pod) {
 	reservedResources := CapacityGroupResources(reservation)
 	allocated := poolV1.ComputeResource{}
@@ -169,6 +288,19 @@ func buildTroughUsage(snapshot *resourcepool.ResourceSnapshot) poolV1.ComputeRes
 	for _, pod := range snapshot.PodSnapshot.ScheduledByName {
 		if poolPod.IsPodPreemptible(pod) {
 			sum = sum.Add(poolPod.FromPodToComputeResource(pod))
+		}
+	}
+	return sum
+}
+
+func buildTroughUsageOnActiveNodes(snapshot *resourcepool.ResourceSnapshot) poolV1.ComputeResource {
+	sum := poolV1.Zero
+	for _, pod := range snapshot.PodSnapshot.ScheduledByName {
+		if poolPod.IsPodPreemptible(pod) {
+			if n := snapshot.NodeSnapshot.AllByName[pod.Spec.NodeName]; n != nil &&
+				node.IsNodeAvailableForScheduling(n, time.Now(), 0) {
+				sum = sum.Add(poolPod.FromPodToComputeResource(pod))
+			}
 		}
 	}
 	return sum
